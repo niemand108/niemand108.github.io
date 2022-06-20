99bd71d00225b124a657061dbd6bdc0ebc4cf5e2
Freezer <santilozano@gmail.com>
2022-06-20 13:35:08 +0200

    fixing overlap

--- F1/2022-CANADA/misc.js
+++ F1/2022-CANADA/misc.js
@@ -1,4 +1,4 @@
 var drivers_misc = ['ALO', 'VER', 'HAM'];
-var turns_misc = ['13-14', '1-2', '6-7', '8-9', '10', '11-12'];
+var turns_misc = ['1-2', '6-7', '8-9', '10', '11-12', '13-14'];
 var name_circuit = 'CANADA';
 var number_season = '2022';
